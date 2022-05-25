63772601d62a43afa99bb18bf0dfe3f9997cf01f
himanshu-suthar-tagline <himanshu.tagline@gmail.com>
2022-05-25 17:14:31 +0530

    Allow users to publish content on instagram through app

--- publishmedia/app.py
+++ publishmedia/app.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from publish_content_all_users import *
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = os.environ.get("UPLOAD_FOLDER")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///user.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "dev123455"
 db = SQLAlchemy(app)
+
+LOGIN = False
+CLIENT = None
 
 
 class InstagramUser(db.Model):
@@ -16,20 +25,90 @@ class InstagramUser(db.Model):
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+
+    # If user is not login with his/her IG username password, 
+    # then he/she will be redirect to the login page
+    if not LOGIN:
+        return redirect(url_for("login_user"))
+
     if request.method == "POST":
         file = request.files["file"]
         content_publish_type = request.form["select"]
-        return "WIP"
+        
+        if 'file' not in request.files:
+            flash('No file part', category="danger")
+            return redirect(request.url)
+
+        if content_publish_type == "Carousel Object":
+            image_files = []
+            for file in request.files.getlist("file"):
+                if file.filename == "":
+                    continue
+                file_save_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+                file.save(file_save_path)
+                image_files.append(file_save_path)
+            
+            if image_files == []:
+                flash("Please upload atleast one file to upload carousel object!", category="warning")
+                return redirect(url_for("index.html"))
+
+            message = upload_carousel_object(CLIENT, image_files, caption="Jay Shree Ram!")
+            flash(message, category="success")
+        
+        elif content_publish_type != "":
+            file_save_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(file_save_path)
+
+            if content_publish_type == "Image Post":
+                message = upload_image_post(CLIENT, file_save_path, caption="Jay Shree Ram!")
+                flash(message, category="success")
+
+            elif content_publish_type == "Image Story":
+                message = upload_photo_to_story(CLIENT, file_save_path, caption="Jay Shree Ram!")
+                flash(message, category="success")
+
+            elif content_publish_type == "Video Post":
+                message = upload_video_post(CLIENT, file_save_path, caption="Jay Shree Ram!")
+                flash(message, category="success")
+
+            elif content_publish_type == "IGTV":
+                message = upload_igtv(CLIENT, file_save_path, caption="Jay Shree Ram!")
+                flash(message, category="success")
+                
+        
     return render_template("index.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login_user():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
         client, response = login(username, password)
+
+        # response return from instagrapi login() module is boolean, it is set to global variable LOGIN
+        # If LOGIN is true then & only then the user can access the index page.
+        global LOGIN
+        LOGIN = response
         if response:
+            global CLIENT
+            CLIENT = client
+            try:
+                # If Instagram login successfull, find the user user in database if exists 
+                # and if he/she has changed the password then update the password with new one
+                # and redirect to the index page
+                user = InstagramUser.query.filter_by(username=username).first()
+                if user.password != password:
+                    user.password = password
+                    db.session.commit()
+                if user:
+                    return redirect(url_for("index"))
+
+            except Exception as e:
+                logging.error(e)
+            
+            # Save the username and password of user to the database
+            # TODO: Hash the password
             entry = InstagramUser(username=username, password=password)
             db.session.add(entry)
             db.session.commit()
--- publishmedia/publish_content_all_users.py
+++ publishmedia/publish_content_all_users.py
@@ -15,13 +15,14 @@ def login(username, password):
     try:
         logging.info("Login Successfull!")
         resp = cl.login(username, password)
+        
     except:
         resp = False
         raise RuntimeError("Provide Username and password or may be they are invalid")
     return cl, resp
 
 
-def upload_photo_to_story(cl):
+def upload_photo_to_story(cl, file_path, caption):
     """
 
     IMAGE_PATH_TO_STORY: This is an environment variable, which is declared in the .env file.
@@ -34,19 +35,19 @@ def upload_photo_to_story(cl):
     """
     try:
         logging.info("\nStart uploading image to story...")
-        cl.photo_upload_to_story(
-            os.environ.get("IMAGE_PATH_TO_STORY"), caption="Jay Shree Ram!"
-        )
-        logging.info("upload image to story successfully!")
+        cl.photo_upload_to_story(file_path, caption=caption)
+        logging.info("Upload image to story successfully!")
+        return "Upload image to story successfully!"
+
     except Exception as e:
-        print(e)
         logging.info("Failed to upload image to story!")
         logging.error(
             "Please provide image path or may be you've not passed the image object!"
         )
+        return "Failed to upload image to story!"
 
 
-def upload_image_post(cl):
+def upload_image_post(cl, file_path, caption):
     """
 
     Method: photo_upload(path: Path, caption: str, upload_id: str, usertags: List[Usertag], location: Location, extra_data: Dict = {})
@@ -55,18 +56,17 @@ def upload_image_post(cl):
     """
     try:
         logging.info("\nStart uploading image as post...")
-        cl.photo_upload(
-            os.environ.get("IMAGE_PATH_TO_POST"),
-            os.environ.get("CAPTION"),
-        )
-        logging.info("upload image as post successfully!")
+        cl.photo_upload(file_path, caption=caption)
+        logging.info("Upload image as post successfully!")
+        return "upload image as post successfully!"
+
     except Exception as e:
-        print(e)
         logging.info("Failed to upload image as post!")
         logging.error("May be provided path is not image type!")
+        return "Failed to upload image as post!"
 
 
-def upload_video_post(cl):
+def upload_video_post(cl, file_path, caption):
     """
 
     Method: video_upload(path: Path, caption: str, thumbnail: Path, usertags: List[Usertag], location: Location, extra_data: Dict = {})
@@ -75,18 +75,17 @@ def upload_video_post(cl):
     """
     try:
         logging.info("\nStart uploading video as post...")
-        cl.video_upload(
-            os.environ.get("VIDEO_PATH_TO_POST"),
-            os.environ.get("CAPTION"),
-        )
-        logging.info("upload video as post successfully!")
+        cl.video_upload(file_path, caption=caption)
+        logging.info("Upload video as post successfully!")
+        return "Upload video as post successfully!"
+
     except Exception as e:
-        print(e)
         logging.info("Failed to upload video as post!")
         logging.error("May be provided path is not video type!")
+        return "Failed to upload video as post!"
 
 
-def upload_carousel_object(cl):
+def upload_carousel_object(cl, file_path_list, caption):
     """
 
     Method: album_upload(paths: List[Path], caption: str, usertags: List[Usertag], location: Location, extra_data: Dict = {})
@@ -95,22 +94,17 @@ def upload_carousel_object(cl):
     """
     try:
         logging.info("\nStart uploading multiple images to a post...")
-        cl.album_upload(
-            [
-                os.environ.get("MEDIA_URL_1"),
-                os.environ.get("MEDIA_URL_2"),
-                os.environ.get("MEDIA_URL_3"),
-            ],
-            os.environ.get("CAPTION"),
-        )
-        logging.info("upload multiple images to a post successfully!")
+        cl.album_upload(file_path_list, caption=caption)
+        logging.info("Upload multiple images to a post successfully!")
+        return "Upload multiple images to a post successfully!"
+
     except Exception as e:
-        print(e)
         logging.info("Failed to upload multiple images to a post!")
         logging.error("May be provided paths are not valid!")
+        return "Failed to upload multiple images to a post!"
 
 
-def upload_igtv(cl):
+def upload_igtv(cl, file_path, caption):
     """
 
     Method: igtv_upload(path: Path, title: str, caption: str, thumbnail: Path, usertags: List[Usertag], location: Location, extra_data: Dict = {})
@@ -119,21 +113,11 @@ def upload_igtv(cl):
     """
     try:
         logging.info("\nStart uploading igtv...")
-        cl.igtv_upload(
-            os.environ.get("IGTV_PATH"),
-            title="Jay Shree Ram",
-            caption=os.environ.get("CAPTION"),
-        )
-        logging.info("upload igtv successfully!")
+        cl.igtv_upload(file_path, title="Jay Bajarang Bali", caption=caption)
+        logging.info("Upload igtv successfully!")
+        return "Upload igtv successfully!"
+
     except Exception as e:
-        print(e)
         logging.info("Failed to upload igtv!")
         logging.error("May be provided path is not valid for IGTV!")
-
-
-# client = login()
-# upload_photo_to_story(client)
-# upload_image_post(client)
-# upload_video_post(client)
-# upload_carousel_object(client)
-# upload_igtv(client)
+        return "Failed to upload igtv!"
